38518188ca681e0798e98a173fdcfcb5fd9564bf
vitalymatyushik <vitaly.matyushik@freighthub.com>
2020-04-26 19:08:02 +0200

    Add divide list with log

--- 6/ZipList.hs
+++ 6/ZipList.hs
@@ -5,3 +5,12 @@ import Control.Applicative (ZipList(ZipList), getZipList)
 
 (>*<) :: [a -> b] -> [a] -> [b]
 (>*<) = \xs ys -> getZipList $ ZipList xs <*> ZipList ys
+
+-- Divide List 1.2.7
+divideList :: Fractional a => [a] -> a
+divideList []     = 1
+divideList (x:xs) = (/) x (divideList xs)
+
+divideList' :: (Show a, Fractional a) => [a] -> (String,a)
+divideList' []     = ("1.0", 1.0)
+divideList' (x:xs) = (/) <$> ("<-" ++ show x ++ "/", x) <*> (divideList' xs)
