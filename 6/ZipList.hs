8b7f1e497b688e7683690b53815ceba8e3e53bca
vitalymatyushik <vitaly.matyushik@freighthub.com>
2020-04-26 22:19:39 +0200

    Add arr2 and arr3

--- 6/ZipList.hs
+++ 6/ZipList.hs
@@ -14,3 +14,21 @@ divideList (x:xs) = (/) x (divideList xs)
 divideList' :: (Show a, Fractional a) => [a] -> (String,a)
 divideList' []     = ("1.0", 1.0)
 divideList' (x:xs) = (/) <$> ("<-" ++ show x ++ "/", x) <*> (divideList' xs)
+
+-- Arr2 and Arr3 1.2.9
+newtype Arr2 e1 e2 a = Arr2 { getArr2 :: e1 -> e2 -> a }
+newtype Arr3 e1 e2 e3 a = Arr3 { getArr3 :: e1 -> e2 -> e3 -> a }
+
+instance Functor (Arr2 e1 e2) where
+  fmap f (Arr2 g) = Arr2 $ \e1 e2 -> f $ g e1 e2
+
+instance Functor (Arr3 e1 e2 e3) where
+  fmap f (Arr3 g) = Arr3 $ \e1 e2 e3 -> f $ g e1 e2 e3
+
+instance Applicative (Arr2 e1 e2) where
+  pure x = Arr2 (\e1 e2 -> x)
+  (Arr2 f) <*> (Arr2 g) = Arr2 (\e1 e2 -> f e1 e2 (g e1 e2))
+
+instance Applicative (Arr3 e1 e2 e3) where
+  pure x = Arr3 (\e1 e2 e3 -> x)
+  (Arr3 f) <*> (Arr3 g) = Arr3 (\e1 e2 e3 -> f e1 e2 e3 (g e1 e2 e3))
